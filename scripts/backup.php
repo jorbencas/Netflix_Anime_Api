5f0eab1fa5c3272960ea46f70e6104fae4b61fd4
jorbencas <jorbencas@gmail.com>
2022-04-29 17:05:21 +0200

    Haciendo cositas con backup

--- scripts/backup.php
+++ scripts/backup.php
@@ -3,15 +3,17 @@
 require_once __DIR__ . '/../classes/api.php';
 $api = new Api('script');
 // Obtener todos los codigos de idioma
-    $sql = "SELECT code FROM langs";
-    $langs = $api->getDb()->listar($sql);
-    $langs = array_map(function($lang){ return $lang->code; }, $langs);
+$sql = "SELECT code FROM langs";
+$langs = $api->getDb()->listar($sql);
+$langs = array_map(function ($lang) {
+    return $lang->code;
+}, $langs);
 // FIN de Obtener todos los codigos de idioma
+$basePath = __DIR__ . "/../media/animes";
 $siglas = __DIR__ . "/../files/animes.json";
 if (file_exists($siglas)) {
     $animes = json_decode(file_get_contents($siglas));
 }
-$basePath = __DIR__ . "/../media/animes";
 $scanedFiles = $api->scanFolders($basePath, false, true);
 if (!file_exists($siglas) || (file_exists($siglas) && sizeof($scanedFiles) > count($animes))) {
     $api->writeFile(json_encode($scanedFiles), '', $siglas);
@@ -19,46 +21,46 @@ if (!file_exists($siglas) || (file_exists($siglas) && sizeof($scanedFiles) > cou
 // $animes = array("5CPS");
 $animes = array("CY");
 foreach ($animes as $siglas) {
-    if (in_array($siglas,array('DC',"nuevos", "INUYASHA", "D01", "Bleach", "AT", "AS", "Haikyuu",
-    "Blood+", "BTB", "DB", "DBS", "DBZ", "DGM", "DN","FMA", "FELE", "FA", "DTB", "GB", "NANA",
-    "IE", "JOJOS", "LLDU", "NNT", "Noragami", "OP", "OYB", "PP", "PPP", "SG", "SD", "SS", "TF",
-    "SX", "TG","TRC"))) {
+    if (!in_array($siglas, array(
+        'DC', "nuevos", "INUYASHA", "D01", "Bleach", "AT", "AS", "Haikyuu",
+        "Blood+", "BTB", "DB", "DBS", "DBZ", "DGM", "DN", "FMA", "FELE", "FA", "DTB", "GB", "NANA",
+        "IE", "JOJOS", "LLDU", "NNT", "Noragami", "OP", "OYB", "PP", "PPP", "SG", "SD", "SS", "TF",
+        "SX", "TG", "TRC"
+    ))) {
         $anime = "$basePath/$siglas";
         $bFolder = "$anime/.backup";
         $api->mkFolders($bFolder);
-        $sqlFile = "$bFolder/sql/$siglas.json";
-        $api->writeFile(json_encode([]), 'backup', $sqlFile);
-        $bNOSQLFolder = "$bFolder/nosql";
-        $api->mkFolders($bNOSQLFolder);
-        $translationsFile = "$bNOSQLFolder/$siglas.json";
-        $api->writeFile(json_encode([]), 'backup', $translationsFile);
-        $mediaFile = "$bNOSQLFolder/$siglas" . "_media.json";
-        $api->writeFile(json_encode([]), 'backup', $mediaFile);
-        if (file_exists($sqlFile) && file_exists($translationsFile) && file_exists($mediaFile)) {
+        $sqlFolder = "$bFolder/sql";
+        $api->mkFolders($sqlFolder);
+        $noSqlFolder = "$bFolder/nosql";
+        $api->mkFolders($noSqlFolder);
+        if (is_dir($sqlFolder) && is_dir($noSqlFolder)) {
             error_log("Creada estructura de ficheros para backup de $siglas - check");
-            backupSql($api, 'animes', $siglas, $anime);
-            backupSql($api, 'seasons', $siglas, $anime);
-            backupSql($api, 'openings', $siglas, $anime);
-            backupSql($api, 'endings', $siglas, $anime);
-            backupSql($api, 'episodes', $siglas, $anime);
-            backupNoSql($api, "media", $siglas, $anime, $langs);
-            backupNoSql($api, "translations", $siglas, $anime, $langs);
+            backupSql($api, 'animes', $siglas, $anime, $sqlFolder);
+            backupSql($api, 'seasons', $siglas, $anime, $sqlFolder);
+            backupSql($api, 'openings', $siglas, $anime, $sqlFolder);
+            backupSql($api, 'endings', $siglas, $anime, $sqlFolder);
+            backupSql($api, 'episodes', $siglas, $anime, $sqlFolder);
+            backupNoSql($api, "translations", $siglas, $anime, $langs, $noSqlFolder);
+            backupNoSql($api, "media", $siglas, $anime, $langs, $noSqlFolder);
         }
     }
 }
 
-function backupSql($api, $kindBackup, $siglas, $anime){
+function backupSql($api, $kindBackup, $siglas, $anime, $sqlFolder)
+{
     $contentFile = array();
-    $sqlFile = "$anime/.backup/sql/$kindBackup.json";
+    $sqlFile = "$sqlFolder/$kindBackup.json";
     if ($kindBackup == 'animes') {
-        $scanedFiles = $api->scanFolders("$anime/episodes");
-        if (sizeof($scanedFiles) > 10) {
+        $numEpisodes = $api->scanFolders("$anime/episodes");
+        if (sizeof($numEpisodes) > 10) {
             $kind = 'serie';
-        } else if (sizeof($scanedFiles) > 1 && sizeof($scanedFiles) < 10) {
+        } else if (sizeof($numEpisodes) > 1 && sizeof($numEpisodes) < 10) {
             $kind = 'ova';
         } else {
             $kind = 'pelicula';
         }
+        error_log("\n \t Creando estructura $kindBackup");
         $contentFile = new stdClass();
         $contentFile->siglas = $siglas;
         $contentFile->generes = '';
@@ -69,144 +71,155 @@ function backupSql($api, $kindBackup, $siglas, $anime){
         $contentFile->kind = $kind;
         $contentFile->temporada  = '';
     } else if ($kindBackup == 'seasons') {
-        $scanedFiles = $api->scanFolders("$anime/episodes",false, true);
+        $scanedFiles = $api->scanFolders("$anime/episodes", false, true);
         if (sizeof($scanedFiles) > 1) {
             //regex [0-9]{2,3}\.(mp4)
+            error_log("\n");
             foreach ($scanedFiles as $file) {
-                if(preg_match("/^[0-9]{1,2}/i",$file)){
-                    // error_log("Session: $siglas");
-                    $idExternal = $siglas . $file;
+                if (preg_match("/^[0-9]{1,2}/i", $file)) {
+                    error_log("\t Creando estructura $kindBackup para $file");
                     $content = new stdClass();
-                    $content->id = $idExternal;
-                    array_push($contentFile,$content);
+                    $content->id = $siglas . $file;
+                    array_push($contentFile, $content);
                 }
             }
         } else if (sizeof($scanedFiles) > 0) {
+            error_log("\n");
             $file = current($scanedFiles);
-            if(preg_match("/^[0-9]{1,2}/i",$file)){
-                $idExternal = $siglas . $file;
+            if (preg_match("/^[0-9]{1,2}/i", $file)) {
+                error_log("\t Creando estructura $kindBackup para $file");
                 $contentFile = new stdClass();
-                $contentFile->id = $idExternal;
+                $contentFile->id = $siglas . $file;
             }
         }
     } else if ($kindBackup == 'openings' || $kindBackup == 'endings') {
         if (is_dir("$anime/$kindBackup")) {
             $scanedFiles = $api->scanFolders("$anime/$kindBackup", false, true);
             if (sizeof($scanedFiles) > 1) {
+                error_log("\n");
                 foreach ($scanedFiles as $file) {
+                    error_log("\t Creando estructura $kindBackup para $file");
                     $content = new stdClass();
-                    $elements = explode("/",$file);               
-                    $fileName = explode('.',end($elements));
-                    $num = preg_replace("/^0{1}/i", "" ,$fileName[0]);
-                    $content->id = $siglas.$num;;
+                    $elements = explode("/", $file);
+                    $fileName = explode('.', end($elements));
+                    $num = preg_replace("/^0{1}/i", "", $fileName[0]);
+                    $content->id = $siglas . $num;
                     $content->nombre = '';
                     $content->descripcion = '';
                     $content->anime = $siglas;
-                    $content->season = '';
+                    $content->season = null;
                     $content->num = $num;
-                    array_push($contentFile,$content);
+                    array_push($contentFile, $content);
                 }
             } else if (sizeof($scanedFiles) > 0) {
+                error_log("\n");
                 $file = current($scanedFiles);
-                $elements = explode("/",$file);               
-                $fileName = explode('.',end($elements));
-                $num = preg_replace("/^0{1}/i", "" ,$fileName[0]);
+                $elements = explode("/", $file);
+                $fileName = explode('.', end($elements));
+                $num = preg_replace("/^0{1}/i", "", $fileName[0]);
+                error_log("\t Creando estructura $kindBackup para $file");
                 $contentFile = new stdClass();
-                $contentFile->id = $siglas.$num;;
+                $contentFile->id = $siglas . $num;
                 $contentFile->nombre = '';
                 $contentFile->descripcion = '';
                 $contentFile->anime = $siglas;
-                $contentFile->season = '';
+                $contentFile->season = null;
                 $contentFile->num = $num;
             }
         }
     } else if ($kindBackup == 'episodes') {
         $scanedFiles = $api->scanFolders("$anime/episodes");
         if (sizeof($scanedFiles) > 1) {
+            error_log("\n");
             $idExternal = $siglas;
             foreach ($scanedFiles as $file) {
                 $info = pathinfo($file);
-                $f = explode("/",$info['dirname']);
-                if(preg_match("/^[0-9]{1,2}/i",end($f))){
-                    $idExternal .= preg_replace("/^0{1}/i", "" ,end($f));
+                $f = explode("/", $info['dirname']);
+                if (preg_match("/^[0-9]{1,2}/i", end($f))) {
+                    $idExternal .= preg_replace("/^0{1}/i", "", end($f));
                 } else {
                     $idExternal = null;
                 }
-                $elements = explode("/",$file);               
-                $fileName = explode('.',end($elements));
-                $num = preg_replace("/^0{1}/i", "" ,$fileName[0]);
+                $elements = explode("/", $file);
+                $fileName = explode('.', end($elements));
+                $num = preg_replace("/^0{1}/i", "", $fileName[0]);
+                error_log("\t Creando estructura $kindBackup para " . end($elements));
                 $content = new stdClass();
-                $content->id = $siglas.$num;;
+                $content->id = $siglas . $num;
                 $content->anime = $siglas;
                 $content->season = $idExternal;
                 $content->num = $num;
-                array_push($contentFile,$content);
+                array_push($contentFile, $content);
             }
         } else if (sizeof($scanedFiles) > 0) {
+            error_log("\n");
             $idExternal = $siglas;
             $file = current($scanedFiles);
             $info = pathinfo($file);
-            $f = explode("/",$info['dirname']);
-            //regex [0-9]{2,3}\.(mp4)
-            if(preg_match("/^[0-9]{1,2}/i",end($f))){
-                $idExternal .= preg_replace("/^0{1}/i", "" ,end($f));
+            $f = explode("/", $info['dirname']);
+            if (preg_match("/^[0-9]{1,2}/i", end($f))) {
+                $idExternal .= preg_replace("/^0{1}/i", "", end($f));
             } else {
                 $idExternal = null;
             }
-            $elements = explode("/",$file);             
-            $fileName = explode('.',end($elements));
-            $num = preg_replace("/^0{1}/i", "" ,$fileName[0]);
+            $elements = explode("/", $file);
+            $fileName = explode('.', end($elements));
+            $num = preg_replace("/^0{1}/i", "", $fileName[0]);
+            error_log("\t Creando estructura $kindBackup para " . end($elements));
             $contentFile = new stdClass();
-            $contentFile->id = $siglas.$num;
+            $contentFile->id = $siglas . $num;
             $contentFile->anime = $siglas;
             $contentFile->season = $idExternal;
             $contentFile->num = $num;
         }
     }
-    if ( (gettype($contentFile) == 'array' && sizeof($contentFile) > 0) || (gettype($contentFile) == 'object' && (isset($contentFile->id) ||isset($contentFile->siglas) ))) {
-        error_log(json_encode($contentFile));
-        $api->writeFile(json_encode($contentFile, JSON_UNESCAPED_UNICODE), 'backup', $sqlFile);
+    if ((gettype($contentFile) == 'array' && sizeof($contentFile) > 0) || (gettype($contentFile) == 'object' && (isset($contentFile->id) || isset($contentFile->siglas)))) {
+        $api->writeFile(json_encode($contentFile, JSON_UNESCAPED_UNICODE), '', $sqlFile);
+        if (file_exists($sqlFile)) {
+            error_log("Creado fichero sql para $kindBackup de $siglas - check");
+        } else {
+            error_log(json_encode($contentFile));
+        }
     } else {
         error_log("No hay $kindBackup en $siglas");
     }
 }
 
-function backupNoSql($api, $kindBackup, $siglas, $anime, $langs){
-    $bNOSQLFile = "$anime/.backup/nosql/$siglas"."_"."$kindBackup.json";;
+function backupNoSql($api, $kindBackup, $siglas, $anime, $langs, $noSqlFolder){
+    $bNOSQLFile = "$noSqlFolder/$kindBackup.json";
     $contentFile = array();
     if ($kindBackup == 'media') {
-        $scanedFiles = $api->scanFolders($anime, true);
-        if (sizeof($scanedFiles) > 0) {
-            foreach ($scanedFiles as $fol) {
-                if( is_file($fol) && !strstr($fol, '.backup')){
-                    $elements = explode("/",$fol);
-                    $fileName = explode('.',end($elements));
-                    $kinds = array('portada','banner','openings','endings','episodes');
-                    foreach ($kinds as $kind) {
-                        if (strstr($fol, $kind)) {
-                            $idExternal = $siglas;
-                            if ($kind !== 'portada' || $kind !== 'banner') {
-                                $fileName = explode('.',end($elements));
-                                if(preg_match('/^0{1}[0-9]/i',$fileName[0])){
-                                    $idExternal .= preg_replace("/^0{1}/i", "" ,$fileName[0]);
-                                }
-                            }
-                            array_push($contentFile,array(
-                                "type" => $kind,
-                                "name" => $fileName[0],
-                                "extension" => $fileName[1],
-                                "id_external" => $idExternal
-                            ));
+        $kinds = array('portada', 'banner', 'openings', 'endings', 'episodes');
+        foreach ($kinds as $kind) {
+            $path = "$anime/$kind";
+            if (is_dir($path)) {
+                error_log("\n");
+                $scanedFiles = $api->scanFolders($path, true, true);
+                if (sizeof($scanedFiles) > 0) {
+                    foreach ($scanedFiles as $fol) {
+                        $fileName = explode('.', $fol );
+                        $idExternal = $siglas;
+                        if ($kind !== 'portada' && $kind !== 'banner') {
+                            $idExternal .= preg_replace("/^0{1}/i", "", $fileName[0]);
                         }
+                        error_log("\t Creando estructura $kindBackup para $kind  ". $fol);
+                        array_push($contentFile, array(
+                            "type" => $kind,
+                            "name" => $fileName[0],
+                            "extension" => $fileName[1],
+                            "id_external" => $idExternal
+                        ));
                     }
                 }
             }
         }
-    } else if($kindBackup == 'translations') {
-        $kinds = array('titulo','sinopsis');
+    } else if ($kindBackup == 'translations') {
+        $kinds = array('titulo', 'sinopsis');
         foreach ($kinds as $kind) {
+            error_log("\n");
             foreach ($langs as $lang) {
-                array_push($contentFile,array(
+                error_log("\t Creando estructura $kindBackup para $kind con idioma $lang");
+                array_push($contentFile, array(
                     "translation" => '',
                     "lang" => $lang,
                     "kind" => $kind,
@@ -214,36 +227,35 @@ function backupNoSql($api, $kindBackup, $siglas, $anime, $langs){
                 ));
             }
         }
-
-        $scanedFiles = $api->scanFolders("$anime/episodes",false, true);
+        error_log("\n");
+        $scanedFiles = $api->scanFolders("$anime/episodes", false, true);
         if (sizeof($scanedFiles) > 0) {
-            //regex [0-9]{2,3}\.(mp4)
             foreach ($scanedFiles as $file) {
-                if(preg_match("/^[0-9]{1,2}/i",$file)){
-                    $idExternal = $siglas . $file;
+                if (preg_match("/^[0-9]{1,2}/i", $file)) {
                     foreach ($langs as $lang) {
-                        array_push($contentFile,array(
+                        error_log("\t Creando estructura $kindBackup para sessions $file con idioma $lang");
+                        array_push($contentFile, array(
                             "translation" => '',
                             "lang" => $lang,
                             "kind" => 'sessions',
-                            "id_external" => $idExternal
+                            "id_external" => $siglas . $file
                         ));
                     }
                 }
             }
         }
-
+        error_log("\n");
         $scanedFiles = $api->scanFolders("$anime/episodes");
         if (sizeof($scanedFiles) > 0) {
             foreach ($scanedFiles as $file) {
+                error_log("\n");
                 $idExternal = $siglas;
-                $info = pathinfo($file);
-                $f = explode("/",$info['dirname']);
-                if(preg_match("/^[0-9]{1,2}/i",end($f))){
-                    $idExternal .= preg_replace("/^0{1}/i", "" ,end($f));
-                }
+                $elements = explode("/", $file);
+                $fileName = explode('.', end($elements));
+                $idExternal .= preg_replace("/^0{1}/i", "", $fileName[0]);
                 foreach ($langs as $lang) {
-                    array_push($contentFile,array(
+                    error_log("\t Creando estructura $kindBackup para episodes " . end($elements) . " con idioma $lang");
+                    array_push($contentFile, array(
                         "translation" => '',
                         "lang" => $lang,
                         "kind" => 'epititulo',
@@ -254,12 +266,15 @@ function backupNoSql($api, $kindBackup, $siglas, $anime, $langs){
         }
     }
     if (sizeof($contentFile) > 0) {
-        error_log(json_encode($contentFile));
-        $api->writeFile(json_encode($contentFile, JSON_UNESCAPED_UNICODE), 'backup', $bNOSQLFile);
+        $api->writeFile(json_encode($contentFile, JSON_UNESCAPED_UNICODE), '', $bNOSQLFile);
+        if (file_exists($bNOSQLFile)) {
+            error_log("Creado fichero nosql para $kindBackup de $siglas - check");
+        } else {
+            error_log(json_encode($contentFile));
+        }
     } else {
         error_log("No hay $kindBackup en $siglas");
     }
-    
 }
 
 // function reloadBackup($animes, $basePath, $api){
@@ -279,4 +294,3 @@ function backupNoSql($api, $kindBackup, $siglas, $anime, $langs){
 //         }
 //     }
 // }
-?>
